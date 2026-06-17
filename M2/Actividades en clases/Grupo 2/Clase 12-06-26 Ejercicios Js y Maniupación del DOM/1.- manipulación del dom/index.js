9baa5012b5190f5fd0de117e290dba0bf97a1fd0
Ricardo Vega <rvega@dataqu.cl>
2026-06-16 20:47:46 -0400

    Actualización clase manipulación del DOM

--- M2/Actividades en clases/Grupo 2/Clase 12-06-26 Ejercicios Js y Maniupación del DOM/1.- manipulación del dom/index.js
+++ M2/Actividades en clases/Grupo 2/Clase 12-06-26 Ejercicios Js y Maniupación del DOM/1.- manipulación del dom/index.js
@@ -43,7 +43,7 @@ console.log(titulo.textContent);//textContent método que me permite recuperar e
 const btnCambiarTitulo = document.getElementById("boton-cambiar-titulo");
 console.log(btnCambiarTitulo);
 
-btnCambiarTitulo.addEventListener("click", function(){
+btnCambiarTitulo.addEventListener("click", function () {
     /*👉 textContent Lee o modifica TODO el texto real del elemento,
     tal como está en el HTML, sin importar estilos. 
 
@@ -80,12 +80,12 @@ console.log(textos);
 const btnCambiarTextos = document.getElementById("boton-cambiar-parrafos");
 
 
-btnCambiarTextos.addEventListener("click", function(){
+btnCambiarTextos.addEventListener("click", function () {
     // for(let i=0; i < textos.length; i++){
     //     textos[i].classList.add("resaltar");
     // }
 
-    for(let texto of textos){
+    for (let texto of textos) {
         texto.style.color = "red";
         texto.style.backgroundColor = "blue";
         texto.style.fontSize = "30px";
@@ -98,8 +98,8 @@ console.log(parrafos);
 
 const btnCambiarTextos2 = document.getElementById("boton-cambiar-parrafos2");
 
-btnCambiarTextos2.addEventListener("click", function(){
-    for(let texto of textos){
+btnCambiarTextos2.addEventListener("click", function () {
+    for (let texto of textos) {
         texto.style.color = "green";
         texto.style.fontSize = "50px";
         texto.style.backgroundColor = "orange";
@@ -136,7 +136,7 @@ getElementsByClassName
 const parrafos3 = document.querySelectorAll(".texto");
 const btnCambiarTextos3 = document.querySelector("#boton-cambiar-parrafos3");
 
-btnCambiarTextos3.addEventListener("click", function(){
+btnCambiarTextos3.addEventListener("click", function () {
     parrafos3.forEach(parrafo => {
         parrafo.style.color = "blue";
         parrafo.style.fontSize = "120px";
@@ -158,9 +158,151 @@ let btnSumar = document.querySelector("#btn-mostrar-suma");
 let resultado = document.querySelector("#resultado-suma");
 
 //Paso 2, Darle funcionalidad al botón
-btnSumar.addEventListener("click", function(){
+btnSumar.addEventListener("click", function () {
     let resultadoSuma = Number(numeroUno.value) + Number(numerodos.value);
     console.log(resultadoSuma);
 
     resultado.textContent = `El resultado de la suma es ${resultadoSuma}`;
+});
+
+
+//=============Agregar nodos al DOM=================
+let lista = document.querySelector("#lista");
+let btnAgregarNodo = document.querySelector("#btn-agregar-nodo");
+
+btnAgregarNodo.addEventListener("click", function () {
+    let nuevoItem = document.createElement("li");//Crear un nuevo nodo o elemento
+    nuevoItem.textContent = "Nuevo Nodo!!";
+    lista.appendChild(nuevoItem);//agregar nuevo hijo al nodo lista (ul)
+});
+
+//Otro ejemplo, agregar parrafo
+let contenedorParrafo = document.querySelector("#contenedor-parrafo");
+let btnAgregarParrafo = document.querySelector("#btn-agregar-nodo-parrafo");
+
+btnAgregarParrafo.addEventListener("click", function () {
+    let nuevoParrafo = document.createElement("p");//Crear un nuevo nodo o elemento
+    nuevoParrafo.textContent = "Nuevo Parrafo!!";
+    contenedorParrafo.appendChild(nuevoParrafo);//agregar nuevo hijo al nodo lista (ul)
+});
+
+//========================= Quitar nodos del DOM ===================
+let btnRemove = document.querySelector("#btn-quitar-nodo");
+
+btnRemove.addEventListener("click", function () {
+    if (lista.lastElementChild) {
+        lista.lastElementChild.remove();
+    } else {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) swalWithBootstrapButtons.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+            });
+            else if (result.dismiss === Swal.DismissReason.cancel)
+                /* Read more about handling dismissals below */
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Your imaginary file is safe :)",
+                    icon: "error"
+                });
+        });
+    }
+});
+
+
+let btnRemove2 = document.querySelector("#btn-quitar-nodo2");
+
+btnRemove2.addEventListener("click", function () {
+    if (lista.lastElementChild) {
+        lista.removeChild(lista.lastElementChild);
+    } else {
+
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "error",
+            title: "No existen nodos a eliminar!!"
+        });
+    }
+});
+
+//=========== Eventos básicos del DOM =======
+
+//Evento click
+let btnClick = document.querySelector("#btn-click");
+
+btnClick.addEventListener("click", function(){
+    alert("Botón presionado!!");
+});
+
+//Evento input
+let ingresoTexto = document.querySelector("#ingreso-texto");
+let resultadoInput = document.querySelector("#resultado-input");
+
+
+ingresoTexto.addEventListener("input", function(){
+    console.log(ingresoTexto.value);
+    resultadoInput.textContent = ingresoTexto.value;
+});
+
+//Evento change
+let ingresoTexto2 = document.querySelector("#ingreso-texto2");
+let resultadoInput2 = document.querySelector("#resultado-input2");
+
+
+ingresoTexto2.addEventListener("change", function(){
+    console.log(ingresoTexto2.value);
+    resultadoInput2.textContent = ingresoTexto2.value;
+});
+
+
+let caja = document.querySelector("#caja");
+//Evento mouseover --> mouse entre
+caja.addEventListener("mouseover", function(){
+    caja.style.backgroundColor = "#edc513";
+    caja.style.color = "#f80808";
+    caja.textContent = "Mouse dentro del nodo!!";
+});
+
+//Evento mouseout --> mouse sale
+caja.addEventListener("mouseout", function(){
+    caja.style.backgroundColor = "#054fee";
+    caja.style.color = "#ffffff";
+    caja.textContent = "Mouse fuera del nodo!!";
+});
+
+
+//Evento keydown
+let ingresoTexto3 = document.querySelector("#ingreso-texto3");
+let resultadoInput3 = document.querySelector("#resultado-input3");
+
+
+ingresoTexto3.addEventListener("keydown", function(event){
+    console.log(`Se presiono una tecla: ${event.key}`);
+    resultadoInput3.textContent = event.key;
 });
